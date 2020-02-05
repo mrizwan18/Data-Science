f22a41950cf89107531d50ee19f57bfdb4fd6e26
Razi <razibhatti1@gmail.com>
2020-02-05 22:40:20 +0500

    Linear Regression working

--- Data Science/Gradient_Descent/gradient_descent.py
+++ Data Science/Gradient_Descent/gradient_descent.py
@@ -1,17 +1,84 @@
 import matplotlib.pyplot as plot  # To plot the data
 import pandas as pd  # For reading data from file
 
-# Reading Data
-data = pd.read_csv('data/data1.txt', sep=",", header=None)
-data.columns = ["population", "profit"]
 
-# Plotting it
-plot.scatter(data['population'], data['profit'], color='r', marker='x')
+def read_data(path):
+    # Reading Data
+    data = pd.read_csv(path, sep=",", header=None)
+    data.columns = ["population", "profit"]
+    return data
 
-plot.title('Visualizing Population vs Profit')
 
-plot.xlabel("Population of City in 10,000's")
+def plot_data(data):
+    plot.scatter(data['population'], data['profit'], color='r', marker='x')
 
-plot.ylabel('Profit in $10,000s')
+    plot.title('Visualizing Population vs Profit')
 
-plot.show()
+    plot.xlabel("Population of City in 10,000's")
+
+    plot.ylabel('Profit in $10,000s')
+
+    # plot.show()
+
+
+def plot_prediction(theta_zero, theta_one, population_vector):
+    # predicted response vector
+    y_pred = theta_zero + theta_one * population_vector
+
+    # plotting the regression line
+    plot.plot(population_vector, y_pred, color="b")
+
+    # putting labels
+    plot.xlabel('x')
+    plot.ylabel('y')
+
+    # function to show plot
+    plot.show()
+
+
+def calculate_error(theta_zero, theta_one, data):
+    m = len(data)
+    error = 0
+    for x, y in data.values:
+        prediction = theta_zero + theta_one * x
+        temp = prediction - y
+        error = error + (temp ** 2)
+    return (1 / (2 * m)) * error
+
+
+def update_thetas(theta_zero_initial, theta_one_initial, alpha, population, profit):
+    N = len(population)
+    error1 = 1
+    error2 = 0.5
+    for i in range(0, len(population)):
+        x = population[i]
+        y = profit[i]
+        prediction = theta_zero_initial + theta_one_initial * x
+        error1 += prediction - y
+        error2 += prediction - y * x
+    theta_zero = theta_zero_initial - (alpha / N) * error1
+    theta_one = theta_one_initial - (alpha / N) * error2
+    return [theta_zero, theta_one]
+
+
+def linear_regression(data):
+    theta_zero = 0
+    theta_one = 0
+    alpha = 0.001
+    population_vector = data['population']
+    profit_vector = data['profit']
+    i = 0
+    prev_err = 10000000
+    while calculate_error(theta_zero, theta_one, data) - prev_err < 0:
+        print("Theeta Zero={0}, theeta One= {1}".format(theta_zero, theta_one))
+        i += 1
+        prev_err = calculate_error(theta_zero, theta_one, data)
+        print("Epoch: {0}  error:  {1}".format(i, prev_err))
+        theta_zero, theta_one = update_thetas(theta_zero, theta_one, alpha, population_vector, profit_vector)
+
+    plot_data(data)
+    plot_prediction(theta_zero, theta_one, population_vector)
+
+
+data = read_data('data/data1.txt')
+linear_regression(data)

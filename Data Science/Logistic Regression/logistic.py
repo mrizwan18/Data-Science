0871602eb885c60b1f80f1d220e6186b116eb99b
Razi <razibhatti1@gmail.com>
2020-02-29 20:08:22 +0500

    Unregularized done

--- Data Science/Logistic Regression/logistic.py
+++ Data Science/Logistic Regression/logistic.py
@@ -1,4 +1,4 @@
-import time as time
+import time
 
 import matplotlib.pyplot as plot  # To plot the data
 import numpy as np
@@ -6,88 +6,59 @@ import pandas as pd  # For reading data from file
 
 
 def read_data(path):
-    """[Function to read data from txt file]
-
-    Arguments:
-        path {[String]} -- [relative path to the dataset.txt]
-
-    Returns:
-        [Pandas Dataframe] -- [Dataframe contains columns of data]
-    """
     # Reading Data
     data = pd.read_csv(path, sep=",", header=None)
     data.columns = ['exam1', 'exam2', 'status']
     return data
 
 
-def plot_data(data):
-    """[Function to plot 2D scatter plot]
-
-    Arguments:
-        data {[Pandas dataframe]} -- [Works only on 2D or 2 feature data]
-    """
+def plot_data_helper(data):
     admitted = data[data.status == 0]
     notAdmitted = data[data.status == 1]
     plot.scatter(admitted.iloc[:, 0], admitted.iloc[:, 1],
-                 color='y', marker='o', edgecolors='black', label='admitted')
+                 color='y', marker='o', edgecolors='black', label='not admitted')
     plot.scatter(notAdmitted.iloc[:, 0], notAdmitted.iloc[:, 1],
-                 color='black', marker='+', edgecolors='black', label='not admitted')
-    plot.legend(loc="upper right")
+                 color='black', marker='+', edgecolors='black', label='admitted')
+    plot.legend(loc="lower left")
 
     plot.title('Data Plot')
 
-    plot.xlabel("Exam1")
+    plot.xlabel("Exam 1")
 
-    plot.ylabel('Exam2')
+    plot.ylabel('Exam 2')
+
+
+def plot_data(data):
+    plot_data_helper(data)
     plot.show()
 
 
-def plot_prediction(thetas, x, data):
-    """[Function to plot predicted hyperplane with respect to original data]
-
-    Arguments:
-        thetas {[array/list of thetas]} -- [Optimal thetas obtained after gradient descent]
-        x {[input vector]} -- [Input/population vector that will help in plotting line]
-    """
-
-    admitted = data[data.status == 0]
-    notAdmitted = data[data.status == 1]
-    plot.scatter(admitted.iloc[:, 0], admitted.iloc[:, 1],
-                 color='y', marker='o', edgecolors='black', label='admitted')
-    plot.scatter(notAdmitted.iloc[:, 0], notAdmitted.iloc[:, 1],
-                 color='black', marker='+', edgecolors='black', label='not admitted')
-    plot.legend(loc="upper right")
-
-    plot.title('Data Plot')
-
-    plot.xlabel("Exam1")
-
-    plot.ylabel('Exam2')
-    # predicted response vector
-    y_pred = np.dot(x, thetas)
-    x = x[:, 1]  # removing the one extra column of all 1
-    # plotting the regression line
-    plot.plot(x, y_pred, color="b")
-
-    # function to show plot
+def plot_prediction(data, thetas, X):
+    plot_data_helper(data)
+    x_value = np.array([np.min(X[:, 1]), np.max(X[:, 1])])
+    y_value = -(thetas[0] + thetas[1] * x_value) / thetas[2]
+    plot.plot(x_value, y_value, "g")
     plot.show()
 
 
 def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def normalize_features(data):
+    feature = data.iloc[:, :-1].to_numpy()
+    y = data.iloc[:, -1].to_numpy()
+    mean = (sum(feature) / len(feature))
+    std = np.std(feature, axis=0)
+    feature = (feature - mean)
+    feature /= std
+    out = np.insert(feature, len(data.columns) - 1, y, axis=1)
+    data = pd.DataFrame(out)
+    data.columns = ['exam1', 'exam2', 'status']
+    return [data, mean, std]
 
 
 def vectorize_data(data, thetas, n):
-    """[function to vectorize data so that iterations are avoided]
-
-    Arguments:
-        data {[pandas dataframe]} -- [contains original data]
-        thetas {list} -- [contains all thetas ]
-        n {[int]} -- [indicates columns of dataset/features of dataset]
-
-    Returns:
-        [array] -- [vectorized x, y and thetas]
-    """
     x = data.iloc[:, :len(data.columns) - 1].to_numpy()  # converting all columns except last one to numpy array
     x = np.insert(x, 0, 1, axis=1)  # appending an all 1 column to start
     y = data.iloc[:, len(data.columns) - 1:].to_numpy()  # converting last one to numpy array
@@ -96,42 +67,18 @@ def vectorize_data(data, thetas, n):
 
 
 def calculate_error(thetas, x, y):
-    """[function to calculate objective function/loss]
-
-    Arguments:
-        thetas {[list]} -- [contains thetas]
-        x {[input vector/matrix]} -- [contains vectorized matrix of input]
-        y {[Y/output matrix]} -- [contains vector of output feature]
-
-    Returns:
-        [int] -- [returns sum of errors for all samples]
-    """
     m = len(x)
-    prediction = np.dot(x, thetas)
-    prediction = sigmoid(prediction)
+    prediction = sigmoid(np.dot(x, thetas))
     c = np.log(prediction)
     c1 = np.log(1 - prediction)
-    error = np.dot(-np.transpose(y), c) - np.dot(np.transpose(1 - y),
-                                                 c1)  # -y(log(h(x)))-(1-y)(log(1-h(x)))
-    return (1 / m) * error
+    error = np.dot(np.transpose(y), c) + np.dot(np.transpose(1 - y),
+                                                c1)  # y(log(h(x)))+(1-y)(log(1-h(x)))
+    return (-1 / m) * error
 
 
 def update_thetas(thetas, alpha, x, y, n):
-    """[function to update theta/gradient descent]
-
-    Arguments:
-        thetas {[list]} -- [contains list of thetas to be updated]
-        alpha {[float]} -- [learning rate]
-        x {[input vector/matrix]} -- [vectorized input matrix]
-        y {[output vector]} -- [vectorized output matrix]
-        n {[int]} -- [no. of columns in dataset]
-
-    Returns:
-        [list] -- [returns updated thetas]
-    """
     N = len(x)
-    prediction = np.dot(x, thetas)
-    prediction = sigmoid(prediction)
+    prediction = sigmoid(np.dot(x, thetas))
     error = (prediction - y) * x
     error = sum(error)
     error = error.reshape(n, 1)
@@ -140,49 +87,37 @@ def update_thetas(thetas, alpha, x, y, n):
 
 
 def logistic_regression_with_gradient(data):
-    """[driver function for logistic regression with gradient_descent]
-
-    Arguments:
-        data {[pandas dataframe]} -- [contains original data]
-
-    Returns:
-        [list] -- [returns optimal thetas and vectorized input]
-    """
-
     n = len(data.columns)
     thetas = np.zeros(n)  # array of thetas
-    alpha = 0.001  # optimal alpha when while loop convergence is used, outputs in 1 seconds
-    # i = 0
-    prev_err = 10000000  # dummy error var to check convergence
-
-    # iterations = 100000 # 90 seconds for 100k iterations
-    # costs = np.zeros(iterations)
+    alpha = 0.1
+    prev_err = 1
+    new_err = 0
     x, y, thetas = vectorize_data(data, thetas, n)
-
-    #          Gradient Descent Method start       ##
-    thetas=[-13.44591237, 0.11275064, 0.1067869]
-    while calculate_error(thetas, x, y) > 0.24:
-        print("error: {0} , thetas: {1}".format(calculate_error(thetas, x, y), thetas))
+    i = 0
+    while prev_err - new_err != 0:
+        i += 1
+        print("Iteration: {0} with prev_err: {1} and new_err: {2}".format(i, prev_err, new_err))
         prev_err = calculate_error(thetas, x, y)
-        # costs = np.insert(costs, i, prev_err, axis=0)
         thetas = update_thetas(thetas, alpha, x, y, n)
+        new_err = calculate_error(thetas, x, y)
+    return thetas, x
 
-    # to plot iterations vs cost function
-    # costs = costs[:iterations, ]
-    # plot.plot(list(range(iterations)), costs, '-r')
-    # plot.show()
 
-    #          Gradient Descent Method End       ##
-    print(calculate_error(thetas, x, y))
-
-    return [thetas, x]
+def predict(thetas, mean, std):
+    x_test = np.array([45, 85])
+    x_test = (x_test - mean) / std
+    x_test = np.append(np.ones(1), x_test)
+    prediction = sigmoid(x_test.dot(thetas))
+    print("Admission Probability is is: {0} %".format(prediction * 100))
 
 
 if __name__ == '__main__':
-    data = read_data("ex2data1.txt")
-    # plot_data(data)
+    original_data = read_data("ex2data1.txt")
+    # plot_data(original_data)
     start_time = time.time()
+    data, mean, std = normalize_features(original_data)
     thetas, x = logistic_regression_with_gradient(data)
     print("--- %s seconds ---" % (time.time() - start_time))
     print(thetas)
-    plot_prediction(thetas, x, data)
+    plot_prediction(data, thetas, x)
+    predict(thetas, mean, std)
